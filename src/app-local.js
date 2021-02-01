b73c9f00d5d547f54735a42f6b746862dc1c1469
Kumindu Induranga Ranawaka <kirlogapanet@gmail.com>
2021-02-01 04:35:44 -0700

    Update app-local.js

--- src/app-local.js
+++ src/app-local.js
@@ -1,6 +1,6 @@
   const app = require('./app');
   const port = 3000;
 
-  app.listen(port, () => {
+  app.listen(port, '0.0.0.0', () => {
       console.log(`listening on http://localhost:${port}`);
   });
